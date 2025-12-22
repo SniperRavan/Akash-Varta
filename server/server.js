6ed11b9764d6666fbe44c58a4a71ee6ce148983e
SniperRavan <akashdasdhibar2003@gmail.com>
2025-12-22 23:56:57 +0530

    Idk what just change here like in the server and cloudinary

--- server/server.js
+++ server/server.js
@@ -100,8 +100,8 @@ io.on("connection", (socket) => {
 
 // Middleware setup
 app.use(cors());
-app.use(express.json({ limit: '15mb' }));
-app.use(express.urlencoded({ extended: true, limit: '15mb' }));
+app.use(express.json({ limit: "15mb" }));
+app.use(express.urlencoded({ extended: true, limit: "15mb" }));
 
 
 // Routes setup
